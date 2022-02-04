bb4dfc5e2601a6c91b1a62090e67af2fe4e0ca43
Vijay Reddy <vivin9090@gmail.com>
2022-02-03 22:37:32 -0500

    problem list

--- leechcode-fe-app/src/app/app.module.ts
+++ leechcode-fe-app/src/app/app.module.ts
@@ -4,13 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
+<<<<<<< Updated upstream
 import { ProblemListComponent } from './components/problem-list/problem-list.component';
+=======
+import { ProblemsListComponent } from './components/problems-list/problems-list.component';
+>>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
+<<<<<<< Updated upstream
     ProblemListComponent
+=======
+    ProblemsListComponent
+>>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
