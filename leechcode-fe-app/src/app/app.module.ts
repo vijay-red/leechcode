b988514c554a79a9e1c01975644d1a1e0a5ed44a
Vijay Reddy <vivin9090@gmail.com>
2022-03-03 02:15:25 -0500

    initial routing

--- leechcode-fe-app/src/app/app.module.ts
+++ leechcode-fe-app/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CodemirrorModule } from '@ctrl/ngx-codemirror';
 import { FormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProblemPageComponent } from './components/problem-page/problem-page.component';
 import { ProblemStatementComponent } from './components/problem-statement/problem-statement.component';
@@ -13,6 +12,15 @@ import { MonacoEditorModule } from '@materia-ui/ngx-monaco-editor';
 
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { ProblemListComponent } from './components/problem-list/problem-list.component';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [
+  {path: 'problem-page', component: ProblemPageComponent},
+  {path: 'problem-list', component: ProblemListComponent},
+  {path: 'home-page', component: HomePageComponent}
+ 
+]
 
 
 @NgModule({
@@ -27,12 +35,14 @@ import { ProblemListComponent } from './components/problem-list/problem-list.com
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    RouterModule.forRoot(routes),
     CodemirrorModule,
     MonacoEditorModule,
     FormsModule
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+export class AppRoutingModule { }
