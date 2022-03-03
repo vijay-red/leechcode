0c60afaeab87fe93647db3f0761126f70f90e787
Erik Rosa <56844083+erik-rosa@users.noreply.github.com>
2022-03-03 01:38:36 -0500

    routing update

--- leechcode-fe-app/src/app/app-routing.module.ts
+++ leechcode-fe-app/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { ProblemPageComponent } from './components/problem-page/problem-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'home', component: HomePageComponent
+}, {path: 'problem', component: ProblemPageComponent 
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [HomePageComponent,ProblemPageComponent]
