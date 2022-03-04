cadd74f0633832a42880d6c961db38b3c6d65378
Harish Reddy <harish0796@gmail.com>
2022-03-03 19:01:35 -0500

    default cors allows all

--- server/handlers/index.go
+++ server/handlers/index.go
@@ -10,17 +10,7 @@ import (
 func RunRouter() {
 	gin.ForceConsoleColor()
 	router := gin.Default()
-	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"https://foo.com"},
-		AllowMethods:     []string{"PUT", "PATCH"},
-		AllowHeaders:     []string{"Origin"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-		AllowOriginFunc: func(origin string) bool {
-			return origin == "https://github.com"
-		},
-		MaxAge: 12 * time.Hour,
-	}))
+	router.Use(cors.Default())
 	routerGroup := router.Group("/api/v1")
 	AddProblemPaths(routerGroup)
 	AddSolutionPaths(routerGroup)
